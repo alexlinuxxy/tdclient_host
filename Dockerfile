f991749f5b79ae1903305b21acc45419fb4c9ae7
Alex Huang <nikshuang@163.com>
2017-06-02 17:06:15 +0800

    rename prebuild.sh to build.sh

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ MAINTAINER "nikshuang@163.com"
 RUN apt-get update && apt-get install git vim make autoconf automake gcc g++ subversion zlib1g-dev libtool -y
 ADD https://nchc.dl.sourceforge.net/project/boost/boost/1.49.0/boost_1_49_0.tar.bz2 /opt/
 RUN git clone https://github.com/openssl/openssl.git /opt/openssl
-COPY prebuild.sh /opt/
+COPY build.sh /opt/
 COPY 30proxy /etc/apt/apt.conf.d/30proxy
 RUN tar xf /opt/boost_1_49_0.tar.bz2 -C /opt/ && rm -f /opt/boost_1_49_0.tar.bz2
--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ OPENSSL=openssl
 BOOST_DIR=/opt/$BOOST
 OPENSSL_DIR=/opt/$OPENSSL
 TD_CLI_SRC_DIR=/mnt/tdclient/ubuntu-12.04
+
 CPUCORES=$((`cat /proc/cpuinfo | grep processor | wc -l` * 2))
 
 find $BOOST_DIR -name *.sh | xargs -I{} chmod +x {}
@@ -15,5 +16,7 @@ export LIBRARY_PATH=$BOOST_DIR:$OPENSSL_DIR
 
 mkdir -p $TD_CLI_SRC_DIR
 cd $TD_CLI_SRC_DIR && svn checkout https://sh-ssvn.sh.intel.com/empg_repos/svn_td/td/trunk/tdclient --username huangshx && patch -p0 < /opt/g++_std.patch
+cd $TD_CLI_SRC_DIR/tdclient/tdclient/libs/ && chmod +x build.ubuntu.sh && ./build.ubuntu.sh
+cd $TD_CLI_SRC_DIR/tdclient/tdclient/linux/ && chmod +x build.ubuntu.sh && ./build.ubuntu.sh
 
 bash
