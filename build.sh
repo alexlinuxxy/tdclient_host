d671c14454dd697b91a467bd402ad756c76b6817
Alex Huang <nikshuang@163.com>
2017-06-02 17:10:27 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -5,16 +5,17 @@ OPENSSL=openssl
 BOOST_DIR=/opt/$BOOST
 OPENSSL_DIR=/opt/$OPENSSL
 TD_CLI_SRC_DIR=/mnt/tdclient/ubuntu-12.04
-
 CPUCORES=$((`cat /proc/cpuinfo | grep processor | wc -l` * 2))
+export BOOST_HOME=$BOOST_DIR
+export LIBRARY_PATH=$OPENSSL_DIR
 
 find $BOOST_DIR -name *.sh | xargs -I{} chmod +x {}
-cd $BOOST_DIR && ./bootstrap.sh && b2 -j$CPUCORES
+cd $BOOST_DIR && ./bootstrap.sh && ./b2 -j$CPUCORES
+ln -s $BOOST_DIR/stage/lib $BOOST_DIR/stage/lib/debian
 cd $OPENSSL_DIR && git checkout OpenSSL_1_0_1c && ./config && make -j$CPUCORES
 
-export LIBRARY_PATH=$BOOST_DIR:$OPENSSL_DIR
-
 mkdir -p $TD_CLI_SRC_DIR
+rm -rf $TD_CLI_SRC_DIR/*
 cd $TD_CLI_SRC_DIR && svn checkout https://sh-ssvn.sh.intel.com/empg_repos/svn_td/td/trunk/tdclient --username huangshx && patch -p0 < /opt/g++_std.patch
 cd $TD_CLI_SRC_DIR/tdclient/tdclient/libs/ && chmod +x build.ubuntu.sh && ./build.ubuntu.sh
 cd $TD_CLI_SRC_DIR/tdclient/tdclient/linux/ && chmod +x build.ubuntu.sh && ./build.ubuntu.sh
