a2bc4c0c7b1f7c4f94f0077126c09692295f4bae
Alex Huang <nikshuang@163.com>
2017-06-02 14:01:13 +0800

    update prebuild.sh

--- prebuild.sh
+++ prebuild.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+BOOST=boost-1.49.0
+OPENSSL=openssl-1.0.1c
+BOOST_DIR=/opt/$BOOST
+OPENSSL_DIR=/opt/$OPENSSL
+CPUCORES=$((`cat /proc/cpuinfo | grep processor | wc -l` * 2))
 
+find $BOOST_DIR -name *.sh | xargs -I{} chmod +x {}
+cd $BOOST_DIR && ./bootstrap.sh && b2 -j$CPUCORES
+cd $OPENSSL_DIR && ./config && make -j$CPUCORES
+
+export LIBRARY_PATH=$BOOST_DIR:$OPENSSL_DIR
+
+bash
