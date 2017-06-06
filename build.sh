e712e345ca7ef317184356a896a76c997ff7f97b
Alex Huang <nikshuang@163.com>
2017-06-06 10:58:44 +0800

    add package

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+OUT_DIR=/mnt
 OPENSSL=openssl
 BOOST=boost_1_49_0
 TARGET=ubuntu-12.04
 BOOST_DIR=/opt/$BOOST
 OPENSSL_DIR=/opt/$OPENSSL
 TD_CLI_SRC_DIR=/mnt/tdclient/$TARGET
+TD_CLI_RELEASE_DIR=$TD_CLI_SRC_DIR/lib/release/tdclient_deb
 CPUCORES=$((`cat /proc/cpuinfo | grep processor | wc -l` * 2))
 
 # boost & openssl
@@ -15,10 +17,14 @@ cd $OPENSSL_DIR && git checkout OpenSSL_1_0_1c && ./config && make -j$CPUCORES
 
 # td
 export BOOST_HOME=$BOOST_DIR
-export LIBRARY_PATH=$OPENSSL_DIR
+export LIBRARY_PATH=$BOOST_DIR/stage/lib:$OPENSSL_DIR
 mkdir -p $TD_CLI_SRC_DIR
 rm -rf $TD_CLI_SRC_DIR/*
-cd $TD_CLI_SRC_DIR && svn checkout https://sh-ssvn.sh.intel.com/empg_repos/svn_td/td/trunk/tdclient --username huangshx
+cd $TD_CLI_SRC_DIR && svn checkout https://sh-ssvn.sh.intel.com/empg_repos/svn_td/td/trunk/tdclient --username huangshx && svn checkout https://sh-ssvn.sh.intel.com/empg_repos/svn_td/td/trunk/build_framework --username huangshx
 cd $TD_CLI_SRC_DIR/tdclient/tdclient/ && patch -p1 < /opt/g++_std.patch
 cd $TD_CLI_SRC_DIR/tdclient/tdclient/libs/ && chmod +x build.ubuntu.sh && sed -i "s/BOOST_HOME=\(.*\)make$/BOOST_HOME=\1make -j$CPUCORES/g" build.ubuntu.sh && ./build.ubuntu.sh
 cd $TD_CLI_SRC_DIR/tdclient/tdclient/linux/ && chmod +x build.ubuntu.sh && sed -i "s/BOOST_HOME=\(.*\)make$/BOOST_HOME=\1make -j$CPUCORES/g" build.ubuntu.sh && ./build.ubuntu.sh
+cd $TD_CLI_SRC_DIR/tdclient/tdclient/linux/ui && chmod +x build.ubuntu.sh && ./build.ubuntu.sh
+
+# package
+cd $TD_CLI_RELEASE_DIR && tar jcf $OUT_DIR/tdcli_$TARGET.tar.bz2 *
