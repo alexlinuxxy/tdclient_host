01317e92592a8b94a3d04f5ee8ca5a2fee1ace8d
Alex Huang <nikshuang@163.com>
2017-06-07 10:45:05 +0800

    fix some bugs

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 OUT_DIR=/mnt
 OPENSSL=openssl
 BOOST=boost_1_49_0
@@ -10,18 +12,18 @@ TD_CLI_SRC_DIR=/mnt/tdclient/$TARGET
 TD_CLI_RELEASE_DIR=$TD_CLI_SRC_DIR/lib/release/tdclient_deb
 CPUCORES=$((`cat /proc/cpuinfo | grep processor | wc -l` * 2))
 
+export BOOST_HOME=$BOOST_DIR
+export LIBRARY_PATH=$BOOST_DIR/stage/lib:$OPENSSL_DIR
+
 # boost & openssl
-cd $BOOST_DIR && patch -p1 </opt/xtime.patch && ./bootstrap.sh --with-libraries="thread,system,serialization,regex,filesystem" && ./b2 -j$CPUCORES
-ln -s $BOOST_DIR/stage/lib $BOOST_DIR/stage/lib/debian
+cd $BOOST_DIR && patch -p1 < /opt/boost.patch && ./bootstrap.sh --with-libraries="thread,system,serialization,regex,filesystem" && ./b2 -j$CPUCORES || 
 cd $OPENSSL_DIR && git checkout OpenSSL_1_0_1c && ./config && make -j$CPUCORES
 
 # td
-export BOOST_HOME=$BOOST_DIR
-export LIBRARY_PATH=$BOOST_DIR/stage/lib:$OPENSSL_DIR
 mkdir -p $TD_CLI_SRC_DIR
 rm -rf $TD_CLI_SRC_DIR/*
 cd $TD_CLI_SRC_DIR && svn checkout https://sh-ssvn.sh.intel.com/empg_repos/svn_td/td/trunk/tdclient --username huangshx && svn checkout https://sh-ssvn.sh.intel.com/empg_repos/svn_td/td/trunk/build_framework --username huangshx
-cd $TD_CLI_SRC_DIR/tdclient/tdclient/ && patch -p1 < /opt/g++_std.patch
+cd $TD_CLI_SRC_DIR/tdclient/tdclient/ && patch -p0 < /opt/tdcli.patch
 cd $TD_CLI_SRC_DIR/tdclient/tdclient/libs/ && chmod +x build.ubuntu.sh && sed -i "s/BOOST_HOME=\(.*\)make$/BOOST_HOME=\1make -j$CPUCORES/g" build.ubuntu.sh && ./build.ubuntu.sh
 cd $TD_CLI_SRC_DIR/tdclient/tdclient/linux/ && chmod +x build.ubuntu.sh && sed -i "s/BOOST_HOME=\(.*\)make$/BOOST_HOME=\1make -j$CPUCORES/g" build.ubuntu.sh && ./build.ubuntu.sh
 cd $TD_CLI_SRC_DIR/tdclient/tdclient/linux/ui && chmod +x build.ubuntu.sh && ./build.ubuntu.sh
